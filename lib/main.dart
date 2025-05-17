3293f1032b0efcb992cf613607d60f7b0b165f3d
tunmdev <tunmdevv@gmail.com>
2025-05-17 17:35:06 +0700

    Init commit

---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:todo_app/utils/theme/theme.dart';
+
+void main() {
+  runApp(const App());
+}
+
+class App extends StatelessWidget {
+  const App({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      themeMode: ThemeMode.system,
+      theme: TAppTheme.lightTheme,
+      darkTheme: TAppTheme.dartTheme,
+    );
+  }
+}
